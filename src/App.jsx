c7079f1585f29570b16440312883860bbbf7b272
gustavo.rañieski <gustavoranieski@hotmail.com>
2023-12-02 14:13:01 -0300

    agregué componentes newcontainer y newarticle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/header"
 import { MainArticle } from "./components/MainArticle"
+import { NewContainer } from "./components/NewContainer"
 
 function App() {
   return (
     <main className="px-4 py-6">
       <Header />
       <MainArticle />
+      <NewContainer />
     </main>
   )
 }
