fb501e54b0cc406f67a7122a3add04f4bd7c51ab
gustavo.rañieski <gustavoranieski@hotmail.com>
2023-12-03 12:01:28 -0300

    estilos responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Header } from "./components/header"
-import { MainArticle } from "./components/MainArticle"
-import { NewContainer } from "./components/NewContainer"
+import { Header } from "./components/header";
+import { MainArticle } from "./components/MainArticle";
+import { NewContainer } from "./components/NewContainer";
 
 function App() {
   return (
     <main className="px-4 py-6">
       <Header />
-      <MainArticle />
-      <NewContainer />
+      <div className='sm:flex sm:gap-8'>
+        <MainArticle />
+        <NewContainer />
+      </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
